29d8de892f81ab82dd8b934cfe69a84172fc25a7
dev-wangliugen <253192463@qq.com>
2017-02-23 10:14:57 +0800

    build frame

---
+++ YLingFM/YLingFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLingFM
+//
+//  Created by 王留根 on 17/2/23.
+//  Copyright © 2017年 ios-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
